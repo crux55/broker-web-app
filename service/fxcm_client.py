1911192c9359e3af5116ed73e362cf7cd4f9b533
quinna2 <andrew.quinn@anz.com>
2020-08-13 11:47:18 +1000

    4 Creating simple buy and sell endpoints

--- service/fxcm_client.py
+++ service/fxcm_client.py
@@ -3,11 +3,17 @@ import fxcmpy
 class FXCMClient:
 
     def __init__(self):
-        self.socket = fxcmpy.fxcmpy(config_file='fxcm.cfg', server='demo')
+        self.socket = fxcmpy.fxcmpy(config_file='config/fxcm.cfg', server='demo', log_level='debug', log_file="fxcm.log")
 
     def create_entry(self, symbol, amount, limit, stop):
-        self.socket.open_trade(symbol=symbol, is_buy=True, is_in_pips=False, amount=amount, time_in_force='GTC',
-                               order_type='AtMarket', limit=limit, stop=stop)
+        trade = self.socket.open_trade(symbol=symbol, is_buy=True,
+                                       is_in_pips=True,
+                                       amount=amount, time_in_force='GTC',
+                                       order_type='AtMarket', limit=limit)
+        print(trade)
+        return trade
 
     def close(self, symbol):
-        self.socket.close_all_for_symbol(symbol)
+        for_symbol = self.socket.close_all_for_symbol(symbol)
+        print(for_symbol)
+        return for_symbol
