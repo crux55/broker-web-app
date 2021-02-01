903e3728fe22c9bc0a15d48039601f1e8f24ee40
crux55 <$Rotaxmax1>
2021-02-01 22:24:49 +1100

    27 - preventing double trades from happening

--- service/fxcm_client.py
+++ service/fxcm_client.py
@@ -6,7 +6,11 @@ class FXCMClient:
         self.socket = fxcmpy.fxcmpy(config_file='config/fxcm.cfg', server='demo')
 
     def open_long(self, trade_request):
-        self.close_shorts(trade_request)
+        if trade_request.flipping:
+            self.close_shorts(trade_request)
+        if not trade_request.allow_multi and self.get_open_longs(trade_request.symbol):
+            return
+
         trade = self.socket.open_trade(symbol=trade_request.symbol, is_buy=True,
                                        is_in_pips=True,
                                        amount=trade_request.amount, time_in_force='GTC',
@@ -49,9 +53,8 @@ class FXCMClient:
             print("Closing long: {}".format(trade))
 
     def close_shorts(self, trade_request):
-        positions = self.socket.get_open_positions()
-        for i in range(len(positions)):
-            if positions.currency[i] in [trade_request.symbol] and not positions.isBuy[i]:
-                trade = self.socket.close_trade(positions.tradeId[i], trade_request.amount, time_in_force='GTC',
-                                                order_type='AtMarket')
-                print("Closing short: {}".format(trade))
+        ids = self.get_open_longs(trade_request.symbol)
+        for i in range(len(ids)):
+            trade = self.socket.close_trade(ids[i], trade_request.amount, time_in_force='GTC',
+                                            order_type='AtMarket')
+            print("Closing short: {}".format(trade))
