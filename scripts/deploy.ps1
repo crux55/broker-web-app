c911e05cafbb192d45658e37f0bb01b433d48c73
crux55 <$Rotaxmax1>
2021-02-01 20:02:04 +1100

    getting back on my feet

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -1,8 +1,2 @@
-$branch= &git rev-parse --abbrev-ref HEAD
-
-if ($branch -ne 'v0.1-dev'){
-    Write-Output 'You can not deploy from a feature'
-    exit
-}
 
 gcloud app deploy --promote --stop-previous-version
