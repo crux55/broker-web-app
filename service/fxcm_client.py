aca3cbce16494f4690c9a8670e623b1999f9dada
crux55 <$Rotaxmax1>
2021-02-02 08:57:15 +1100

    31 - removing stops

--- service/fxcm_client.py
+++ service/fxcm_client.py
@@ -14,8 +14,9 @@ class FXCMClient:
         trade = self.socket.open_trade(symbol=trade_request.symbol, is_buy=True,
                                        is_in_pips=True,
                                        amount=trade_request.amount, time_in_force='GTC',
-                                       order_type='AtMarket', stop=trade_request.stop,
-                                       trailing_step=trade_request.trailing)
+                                       order_type='AtMarket')
+                                        # , stop=trade_request.stop,
+                                       # trailing_step=trade_request.trailing)
         print("Opening long: {}".format(trade))
         return trade
 
@@ -24,8 +25,9 @@ class FXCMClient:
         trade = self.socket.open_trade(symbol=trade_request.symbol, is_buy=False,
                                        is_in_pips=True,
                                        amount=trade_request.amount, time_in_force='GTC',
-                                       order_type='AtMarket', stop=trade_request.stop,
-                                       trailing_step=trade_request.trailing)
+                                       order_type='AtMarket')
+                                       # , stop=trade_request.stop,
+                                       # trailing_step=trade_request.trailing)
         print("Opening short: {}".format(trade))
         return trade
 
